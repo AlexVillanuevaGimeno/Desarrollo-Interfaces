7e2c866b0cf7e5a0bf8403662e7620abe487d792
AlexVillanuevaGimeno <a26954@svalero.com>
2024-03-02 18:58:50 +0100

    updated style Mtto

--- www/di23/vistas/Menus/V_ResultadosMtto.php
+++ www/di23/vistas/Menus/V_ResultadosMtto.php
@@ -3,17 +3,22 @@ function imprimirMenu($menu, $nivel = 0)
 {
     $estilo = '';
     if ($nivel == 1) {
-        $estilo = 'font-size: 2em; font-weight: bold; color: black;  text-shadow: 0 0 16px rgba(255, 247, 103, 1), 0 0 16px rgba(255, 247, 103, 1);';
+        $estilo = 'font-size: 2em; font-weight: bold; color: black;  text-shadow: 0 0 16px rgba(255, 247, 103, 1), 0 0 16px rgba(255, 247, 103, 1); margin-right: 10px;';
     } else {
-        $estilo = 'font-size: 1.5em; font-style: italic;  color: black;  text-shadow: 0 0 16px rgba(255, 247, 103, 1), 0 0 16px rgba(255, 247, 103, 1);';
+        $estilo = 'font-size: 1.5em; font-style: italic;  color: black;  text-shadow: 0 0 16px rgba(255, 247, 103, 1), 0 0 16px rgba(255, 247, 103, 1); margin-right: 10px;';
     }
 
     echo '<div style="border: 1px solid black; background-color: #b88d37; border-radius: 5px; padding: 10px; margin: 10px;">';
-    echo '<span style="' . $estilo . '">' . str_repeat(' ', ($nivel - 1) * 4) . $menu['nombre_menu'] . "</span><br>";
+    echo '<span style="' . $estilo . '">' . str_repeat(' ', ($nivel - 1) * 4) . $menu['nombre_menu'] . "</span>";
+
+    echo '<button style="margin: 5px; ">Crear Menú</button>';
+    echo '<button style="margin: 5px; ">Actualizar Menú</button>';
+    echo '<button style="margin: 5px; ">Eliminar Menú</button>';
+
+    echo '<br>';
     if (!empty($menu['hijos'])) {
         foreach ($menu['hijos'] as $hijo) {
             imprimirMenu($hijo, $nivel + 1);
-            echo '</div>';
         }
     }
     echo '</div>';
